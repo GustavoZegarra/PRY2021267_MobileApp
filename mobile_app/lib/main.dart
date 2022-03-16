9ff6714c3ba274594a6d5aef7f6823fe1a8484eb
FernyHz <u201617247@upc.edu.pe>
2022-03-15 21:20:48 -0500

    Consumiendo una API y refrescando cada N tiempo

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,10 +1,24 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:mobile_app/screens/PrimerMensajeScreen.dart';
 import 'package:mobile_app/screens/request_permission_screen.dart';
 import 'package:mobile_app/screens/screens.dart';
 import 'package:mobile_app/themes/app_theme.dart';
 
-void main() => runApp(const MyApp());
+//para correr un api que no esta en la nube
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() {
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,17 +26,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sistema de Alerta Temprana',
-      initialRoute: 'pMensaje',
-      routes: {
-        'home': (_) => HomeScreen(),
-        'detail': (_) => DetailScreen(),
-        'tab': (_) => TabScreen(),
-        'requestPermission':(_)=>RequestPermissionScreen(),
-        'pMensaje':(_)=>PrimerMensajeScreen()
-      },
-      theme: AppTheme.lightTheme
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Sistema de Alerta Temprana',
+        initialRoute: 'pMensaje',
+        routes: {
+          'home': (_) => HomeScreen(),
+          'detail': (_) => DetailScreen(),
+          'tab': (_) => TabScreen(),
+          'requestPermission': (_) => RequestPermissionScreen(),
+          'pMensaje': (_) => PrimerMensajeScreen()
+        },
+        theme: AppTheme.lightTheme);
   }
 }
--- mobile_app/lib/screens/home_screen.dart
+++ mobile_app/lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:mobile_app/models/quebrada_model.dart';
 import 'package:mobile_app/screens/screens.dart';
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'package:path/path.dart';
 
@@ -13,17 +17,52 @@ class HomeScreen extends StatefulWidget {
 class HomeState extends State<HomeScreen> {
   //const HomeScreen({Key? key}) : super(key: key);
   late List<Quebrada> Quebradas;
+  Timer? timer;
   String quebradas =
-      '[{"nombre": "Quirio", "precipitacion": 5.69698}, {"nombre": "Pedregal ", "precipitacion": 6.3562}, {"nombre": "Rayo de Sol", "precipitacion": 4.2698}]';
+      '[{"nombre": "Quirio", "precipitacion": 5.69698, "ola":"peluca"}, {"nombre": "Pedregal ", "precipitacion": 6.3562}, {"nombre": "Rayo de Sol", "precipitacion": 4.2698}]';
+
+  Future<void> RefreshListaQuebradas() async {
+    //  List ListaJson = jsonDecode(quebradas) as List;
+    //  List<Quebrada> Quebradas2 =
+    //      ListaJson.map((e) => Quebrada.fromJson(e)).toList();
+    //  print(Quebradas2[0].nombre + " " + Quebradas2[0].precipitacion.toString());
+    //'http://10.0.2.2:32837/api/Alerta'
+    //String url = 'http://10.0.2.2:32837/api/Alerta';
+    //final response = await http.get(Uri.parse(url));
+//
+    //if (response.statusCode == 200) {
+    //  print(response.body);
+    //} else {
+    //  print(response.reasonPhrase);
+    //}
+    String ola = "";
+    HttpClient httpClient = new HttpClient();
+    //PARA QUE ESTO FUNCION EN EL ASP NETCORE DEBE ESTAR CORRIENDO DESDE API APPLICATION (PUERTOS 5000 Y 5001) Y NO DESDE ISSL(ACA NO FUNCIONA EN FLUTTER)
+    //https://10.0.2.2:5001/api/Alerta
+    HttpClientRequest request =
+        await httpClient.getUrl(Uri.parse("https://my-json-server.typicode.com/GustavoZegarra/JSONPlaceholder/posts"));
+    //request.headers.add("user-agent", "");
+    HttpClientResponse response = await request.close();
+    ola = await response.transform(utf8.decoder).join();
+    print(ola);
+    httpClient.close();
+  }
 
   @override
   void initState() {
     List ListaJson = jsonDecode(quebradas) as List;
     Quebradas = ListaJson.map((e) => Quebrada.fromJson(e)).toList();
-    print(Quebradas[0].nombre + " " + Quebradas[0].precipitacion.toString());
-
+    //print(Quebradas[0].nombre + " " + Quebradas[0].precipitacion.toString());
+    timer = Timer.periodic(
+        Duration(seconds: 5), (Timer t) => RefreshListaQuebradas());
     super.initState();
     //ScaffoldMessenger.of(this.context).showSnackBar(SnackBar(content: Text("Ayudaaaaaa")));
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 
   @override
